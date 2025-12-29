f65a96c2e3963e83248264bb31bd0571d46a24d6
nadav <nadav@example.com>
2025-12-29 20:43:13 +0200

    initial commit

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "Hello DevOps World! This is version 1."
+
+if __name__ == '__main__':
+    # Run on all interfaces (0.0.0.0) so Docker can expose it
+    app.run(host='0.0.0.0', port=5000)
